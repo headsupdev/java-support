2d03cca99709ce6e5c79d4e5b9197bfd50039833
Robert Hewitt <robert@kotikan.com>
2012-04-20 18:01:57 +0100

    updated calendar to use DATE constant instead of DAY_OF_YEAR constant when adding / subtracting days from the calendar objec

---
+++ src/test/java/org/headsupdev/support/java/DateUtilTest.java
@@ -0,0 +1,13 @@
+package org.headsupdev.support.java;
+
+/**
+ * TODO Add documentation
+ * <p/>
+ * Created: 20/04/2012
+ *
+ * @author roberthewitt
+ * @since xxxxxx
+ */
+public class DateUtilTest
+{
+}
